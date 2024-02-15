45642cc49e908bece6020a03572c2a7e7e8a3a78
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-02-15 14:21:44 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/twelve_hours/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.twelve_hours
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant/color_palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static ColorScheme colorScheme = const ColorScheme.light(
+    primary: primary,
+    secondary: secondary,
+  );
+  static const Color primary = Color(0xff8F00FF);
+  static const Color secondary = Color(0xff8F00FF);
+}
